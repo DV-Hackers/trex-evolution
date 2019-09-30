04f63873d61ab8855862a59074430d5ae9502477
jacobhanson <jhanreg11@gmail.com>
2019-09-29 22:26:38 -0700

    first commit

---
+++ js/nn.js
@@ -0,0 +1,16 @@
+class LiteNN {
+constructor(layers, activation) {
+/*
+layers - a list of ints that signify the number of activations in each layer, first element represents input, last represents output
+activation - activation function for the nn, takes in one number and returns another number
+*/
+}
+ff(x) {
+/*
+x - list representing input into nn
+*/
+}
+
+crossover(other) {}
+mutate() {}
+}
