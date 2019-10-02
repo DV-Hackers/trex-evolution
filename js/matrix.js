d8da37b33cdfaf6a4507374453d70e8fdf088c0d
jacobhanson <jhanreg11@gmail.com>
2019-10-02 13:33:43 -0700

    added matrix.js

---
+++ js/matrix.js
@@ -0,0 +1,12 @@
+class Matrix {
+    constructor(rows, cols) {
+        //This function "constructs" objects, so every time you call "new Matrix(..)" anywhere it executes this function
+        //I want you to initialize a 2d array with the number of rows/cols specified by the parameters passed in. every value can just be 0 for now
+
+    }
+
+    mult(vector) {
+    \\This function takes in a list of numbers and I want you to do to multiply the matrix in question by this vector and return the result
+    \\HINT: make sure to check that vector is the right length, and then just iterate through each row of the matrix and calculate the dot product
+
+    }
