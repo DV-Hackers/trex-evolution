7799f7064dd7f8ed084e7c2de6f039edbc8ed029
jacobhanson <jhanreg11@gmail.com>
2019-10-14 20:16:41 -0700

    created NN constructor

--- js/nn.js
+++ js/nn.js
@@ -1,9 +1,20 @@
+class Matrix {
+
+}
+
 class LiteNN {
 constructor(layers, activation) {
 /*
 layers - a list of ints that signify the number of activations in each layer, first element represents input, last represents output
 activation - activation function for the nn, takes in one number and returns another number
 */
+  this.weights = {new Matrix(layers[1], layers[0])}
+
+  for (var i = 0; i < layers.length - 1; i++) {
+    if (i == layers.length-1)
+      break
+    this.weights.push(new Matrix(layers[i+1], layers[i]))
+  }
 }
 ff(x) {
 /*
