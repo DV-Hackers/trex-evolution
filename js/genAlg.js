6f66f161460ae95831bee62d62be62e0f6362649
reed <reedbertolotti@icloud.com>
2019-12-20 17:01:57 -0800

    fixed remaining bugs

--- js/genAlg.js
+++ js/genAlg.js
@@ -19,64 +19,75 @@ class Population
 
   calcFitness()
   {
-    let totalFit = 0;
     for (let i = 0; i < this.agents.length; i++)
     {
       this.agents[i].calcFit(this.findPhrase);
-      totalFit += this.agents[i].fitness;
-    }
-
-    for (let i = 0; i < this.agents.length; i++)
-    {
-        this.agents[i].fitness = this.agents[i].fitness / totalFit;
     }
   }
 
   buildMatingPool()
   {
-    let amt = 100;
-    for (let i = 0; i < amt; i++)
+    let totalFit = 0;
+    for (let k = 0; k < this.agents.length; k++)
     {
-      let addAmt = this.agents[i].fitness * amt;
+      totalFit += this.agents[k].fitness;
+    }
+
+    let amt = 1000;
+    for (let i = 0; i < this.agents.length; i++)
+    {
+      let addAmt = this.agents[i].fitness / totalFit * amt;
       for (let j = 0; j < addAmt; j++)
         this.matingPool.push(this.agents[i]);
     }
   }
 
-  reproduce(mr)
+  reproduce()
   {
     for (let i = 0; i < this.agents.length; i++)
     {
       let parent1 = this.matingPool[Math.floor(Math.random() * this.matingPool.length)];
       let parent2 = this.matingPool[Math.floor(Math.random() * this.matingPool.length)];
       let child = parent1.crossOver(parent2);
-      child.mutate(mr);
+      child.mutate(this.mutationRate);
       return child;
     }
   }
 
+  evalPop()
+  {
+    for (let i = 0; i < this.agents.length; i++)
+    {
+      if (this.agents[i].fitness === 1)
+        return this.agents[i];
+    }
+    return null;
+  }
+
   newPopulation()
   {
-    let done = false;
+    let evaluation = this.evalPop();
+    if (evaluation != null)
+      return evaluation;
 
-      console.log(this.agents);
-      this.calcFitness();
-      this.buildMatingPool();
-      let newPop = [];
-      for (let i = 0; i < this.agents.length; i++)
-      {
-        let child = this.reproduce(this.mutationRate);
+    this.buildMatingPool();
+    let newPop = [];
+    for (let i = 0; i < this.agents.length; i++)
+    {
+      let child = this.reproduce();
 
-        newPop.push(child);
-      }
-      this.agents = newPop;
+      newPop.push(child);
     }
+    this.agents = newPop;
+    this.calcFitness();
+    return null;
   }
+}
 
 
 class Agent
 {
-  constructor (phraseLen)
+  constructor(phraseLen)
   {
     this.fitness;
     this.phrase = [];
@@ -105,7 +116,6 @@ class Agent
     this.fitness = same / phrase.length;
   }
 
-
   crossOver(other)
   {
     let child = new Agent(this.phrase.length);
@@ -127,7 +137,7 @@ class Agent
   {
     for (let i = 0; i < this.phrase.length; i++)
     {
-      if (Math.random() <= mutationRate)
+      if (Math.random() < mutationRate)
       {
         this.phrase[i] = this.getRandomChar();
       }
@@ -136,8 +146,14 @@ class Agent
 }
 
 //driver code
-console.log("hi");
-let pop = new Population(200, 0.01, "hello world");
-pop.newPopulation();
-for (let i = 0; i < 100; i++)
-  pop.newPopulation();
+console.log("test");
+let pop = new Population(400, 0.01, "hello world");
+let eval = pop.evalPop();
+let x = 0;
+while (eval === null)
+{
+  eval = pop.newPopulation();
+  x++;
+}
+console.log(eval);
+console.log(x);
