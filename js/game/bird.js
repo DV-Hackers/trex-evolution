4eb7a7e48f1802d109acb71b1a21d1fcdd710c60
reed <reedbertolotti@icloud.com>
2020-01-26 21:00:31 -0800

    standardized bird width, altered y value calculation

--- js/game/bird.js
+++ js/game/bird.js
@@ -1,8 +1,15 @@
 class Bird extends Obstacle {
 	constructor() {
 		super()
-		this.width = 10
-		this.y = 5 + Math.floor(Math.random() * 3) * 5
+		this.width = 20 // previously 10
+		this.y =  2 + Math.floor(Math.random() * 24)  /*5 + Math.floor(Math.random() * 3) * 5*/
+		if (this.y < 12 || Math.random() < 0.25)
+		{
+			this.height = 10
+		}
+		else
+			this.height = 50 // added
+
 		this.type = 1;
 	}
 
